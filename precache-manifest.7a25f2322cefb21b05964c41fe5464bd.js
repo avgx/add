087161ca5e75a887e9bf9a5a7478b07c46b352c3
avgx <agovorovsky@gmail.com>
2020-06-27 01:13:40 +0300

    Updates

---
+++ precache-manifest.7a25f2322cefb21b05964c41fe5464bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6f3b73f6a4b54d5b4095e88983dd5f",
+    "url": "/add/index.html"
+  },
+  {
+    "revision": "5a66dc9272f0abd34f4e",
+    "url": "/add/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe61c78e9abf4a6b1a8b",
+    "url": "/add/static/js/2.069f5052.chunk.js"
+  },
+  {
+    "revision": "007c757cb2729d65403f43c49aadaf92",
+    "url": "/add/static/js/2.069f5052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750267028b5e398484be",
+    "url": "/add/static/js/3.eb59ce13.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/add/static/js/3.eb59ce13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a258db9dc186a0d96e76",
+    "url": "/add/static/js/4.7e3e2ef4.chunk.js"
+  },
+  {
+    "revision": "5a66dc9272f0abd34f4e",
+    "url": "/add/static/js/main.8f732f0c.chunk.js"
+  },
+  {
+    "revision": "acbbf98c7dc46ebf71af",
+    "url": "/add/static/js/runtime-main.fc39ff94.js"
+  }
+]);
